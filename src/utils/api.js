d382d045d0a918f7879cfd41346d931be2eabf54
Radishetta <geneshaft@gmail.com>
2024-06-18 11:56:19 +0100

    Adds functionality to add and remove votes from articles showing feedback to user with optimistic rendering

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,19 +3,45 @@ import axios from "axios";
 const ncNewsAPI = axios.create({ baseURL: "https://nutcracker-news-iaiw.onrender.com/api" });
 
 export const getArticles = () => {
-  return ncNewsAPI.get("/articles").then((res) => {
-    return res.data;
-  });
+  return ncNewsAPI
+    .get("/articles")
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return Promise.reject(err.res.data);
+    });
 };
 
 export const getArticleByID = (id) => {
-  return ncNewsAPI.get(`/articles/${id}`).then((res) => {
-    return res.data;
-  });
+  return ncNewsAPI
+    .get(`/articles/${id}`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return Promise.reject(err.res.data);
+    });
 };
 
 export const getCommentsByArticleID = (id) => {
-  return ncNewsAPI.get(`/articles/${id}/comments`).then((res) => {
-    return res.data;
-  });
+  return ncNewsAPI
+    .get(`/articles/${id}/comments`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return Promise.reject(err.res.data);
+    });
+};
+
+export const updateArticleVote = (id, body) => {
+  return ncNewsAPI
+    .patch(`/articles/${id}`, { inc_votes: body })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return Promise.reject(err.res.data);
+    });
 };
